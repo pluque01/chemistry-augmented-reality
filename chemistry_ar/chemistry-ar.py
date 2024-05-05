2e590fb28fe253d804562ac508320eb31038da6e
Pablo Luque <pablols114@gmail.com>
2024-05-05 18:10:10 +0200

    style: move Marker to its own file

--- chemistry_ar/chemistry-ar.py
+++ chemistry_ar/chemistry-ar.py
@@ -2,81 +2,18 @@ import os
 import cv2
 import moderngl_window as mglw
 import moderngl
-from moderngl_window.context.base.window import Tuple
 import numpy as np
-from numpy._typing import ArrayLike
 import camera
 from dotenv import load_dotenv
-from typing import List, Dict
+from typing import Dict, Tuple
 from molecule import Molecule
+from marker import Marker, MarkerState
 
 from shapes.rectangle import Rectangle
-
-from enum import Enum
 
 load_dotenv()
 MARKER_SIZE = 0.48
 DEBUG = os.environ.get("DEBUG", False)
-
-
-class MarkerState(Enum):
-    ACTIVE = 0
-    NOT_FOUND = 1
-    INACTIVE = 2
-
-
-class Marker:
-    def __init__(self, id: int, marker_extrinsics: Tuple[np.ndarray, np.ndarray]):
-        self.id = id
-        self.marker_pos = marker_extrinsics
-        self.state = MarkerState.ACTIVE
-        self.frames_lost = 0
-        self.molecule = None
-
-    def update_marker_pos(self, marker_pos: Tuple[np.ndarray, np.ndarray]):
-        self.marker_pos = marker_pos
-
-    def update_marker_state(self, state: MarkerState):
-        if (
-            self.state == MarkerState.ACTIVE
-            or MarkerState.NOT_FOUND
-            and state == MarkerState.NOT_FOUND
-        ):
-            self.state = MarkerState.NOT_FOUND
-            self.frames_lost += 1
-        if self.state == MarkerState.NOT_FOUND and state == MarkerState.ACTIVE:
-            self.state = MarkerState.ACTIVE
-            self.frames_lost = 0
-
-        if self.frames_lost > 10:
-            self.state = MarkerState.INACTIVE
-
-    def get_marker_pos(self):
-        return self.marker_pos
-
-    def get_marker_state(self):
-        return self.state
-
-    def get_frames_lost(self):
-        return self.frames_lost
-
-    def create_molecule(
-        self,
-        ctx,
-        id: int,
-        marker_pos: Tuple[np.ndarray, np.ndarray],
-        atoms: str,
-        projection_matrix,
-    ):
-        self.molecule = Molecule(ctx, atoms, id, marker_pos, projection_matrix)
-
-    def delete_molecule(self):
-        self.molecule = None
-
-    def render_molecule(self, frame_time: float):
-        if self.molecule is not None:
-            self.molecule.update_marker_extrinsics(self.marker_pos)
-            self.molecule.render(frame_time)
 
 
 class ChemistryAR(mglw.WindowConfig):
